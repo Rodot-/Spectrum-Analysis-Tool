c9ea8dbbff41366dfbf3cb8a7e238e354876729e
Jack O'Brien <jto33@drexel.edu>
2015-02-25 16:02:50 -0500

    Optimized Matching, Header getting

--- DataManager.py
+++ DataManager.py
@@ -4,9 +4,14 @@ import numpy as np
 import math
 import eventlet
 import time
+import MatchingFunctions
+import fileinput
 
-pool = eventlet.GreenPool(size = 2000000)
+BuildFitsList = MatchingFunctions.BuildFitsList
+FHV = MatchingFunctions.FindHeaderValue
 
+pool = eventlet.GreenPool(size = 100)
+'''
 def BuildFitsList(path):
 
 	try:
@@ -47,69 +52,152 @@ def GetRaDec(): #Gets a filename, ra and dec of a spectra for matching
 
 	print "Getting Data"
 
-        RADEC = []
+	temp = np.loadtxt("Matches.rep", delimiter=', ', dtype={'names':["GroupID","MJD","PLATEID","FIBERID","RA","DEC","REDSHIFT","FILENAME","Interesting"], 'formats': ["int","int","int","int","float","float","float","<S64","<S64"]})
+
+        RADEC = [(math.cos(i['RA']), i['DEC'],i['RA'],i['FILENAME'], i['REDSHIFT'], i['MJD'], i['PLATEID'],i['FIBERID']) for i in temp]
+
+	RADEC = list(set(RADEC))
+	
+	Cache = set(temp['FILENAME'])
+
+	print "Looking for New Files"
+
+	SDSSDir = set(os.listdir("downloads/SDSS")) - Cache
+
+	BOSSDir = set(os.listdir("downloads/BOSS")) - Cache
+
+	print len(SDSSDir) + len(BOSSDir), "New Files Found"
 
 	print "Looking For Files in downloads/SDSS..."
 
-	for i in pool.imap(BuildFitsList, os.listdir("downloads/SDSS")):
+	for i in pool.imap(BuildFitsList, SDSSDir):
 		RADEC.append(i)
-		print i[2]
+		#print i
 
 	print "Looking For Files in downloads/BOSS..."
 
-	for i in pool.imap(BuildFitsList, os.listdir("downloads/BOSS")):
+	for i in pool.imap(BuildFitsList, BOSSDir):
 		RADEC.append(i)
-		print i
+		#print i
 
-	print "Done Getting Data"
         return np.array(RADEC,dtype = [('cos(RA)','<f8'),('DEC','<f8'),('RA','<f8'),('FILENAME', '<S64'),('Z','<f4'),('MJD','<i4'),('PLATE','<i4'),('FIBER','<i4')] )
+'''
+def GetRaDec(): #Gets a filename, ra and dec of a spectra for matching
 
+	print("Getting Data")
+
+	try:
+
+		temp = np.loadtxt("Matches.rep", delimiter=', ', dtype={'names':["GroupID","MJD","PLATEID","FIBERID","RA","DEC","REDSHIFT","FILENAME","Interesting"], 'formats': ["int","int","int","int","float","float","float","<S64","<S64"]})
+
+
+		if len(temp) == 0:
+
+			raise Exception("File Has No Data")
+
+		else:
+
+	        	CachedTable = set([(math.cos(i['RA']), i['DEC'],i['RA'],i['FILENAME'], i['REDSHIFT'], i['MJD'], i['PLATEID'],i['FIBERID']) for i in temp]) 
+
+	except:
+
+		print("Could not Load Matches.rep")
+
+		temp = np.zeros((0), dtype = [('cos(RA)','<f8'),('DEC','<f8'),('RA','<f8'),('FILENAME', '<S64'),('Z','<f4'),('MJD','<i4'),('PLATE','<i4'),('FIBER','<i4')])
+		CachedTable = set()
+
+	print("Looking for New Files")
+
+	FileList = set(os.listdir("downloads/SDSS")) - set(temp['FILENAME'])
+
+	#if len(set(os.listdir("downloads/SDSS"))) < len(set(temp['FILENAME'])):
+
+	#	raise Exception("Bad Math on FileList")
+
+	nFiles = len(FileList)
+
+	TotalListLength = nFiles + len(CachedTable)
+
+	print(str(nFiles) + " New Files Found")
+
+	print("Extracting Headers...")
+
+	RADEC = np.empty((TotalListLength), dtype = [('cos(RA)','<f8'),('DEC','<f8'),('RA','<f8'),('FILENAME', '<S64'),('Z','<f4'),('MJD','<i4'),('PLATE','<i4'),('FIBER','<i4')])
+
+	RADEC[0:len(CachedTable)] = list(CachedTable)
+
+	RADEC_index = len(CachedTable)
+
+	t0 = time.time()
+	
+	for i in pool.imap(BuildFitsList, FileList):
+		RADEC[RADEC_index] = i
+		RADEC_index += 1
+		print RADEC_index
+	
+	#for i in iter(FileList):
+	#	RADEC[RADEC_index] = BuildFitsList(i)
+	#	RADEC_index += 1
+	#	#print RADEC_index
+	
+	#RADEC[len(CachedTable):] = [BuildFitsList(i) for i in FileList]
+
+	#print len(RADEC)
+	print("Found " + str(TotalListLength) + " files in " + str(time.time()-t0))
+	#raise Exception("STOP")
+
+	return RADEC
+	
 def groupData(): #matches all data, exports to Matches.rep
 
         X = GetRaDec()
         print "Starting Matching"
         X = np.sort(X, order = 'DEC')
-
 	t0 = time.time()
 
-        output = open('Matches.rep', 'w')
+        output = open('Matches.rep', 'wb')
 
 	output.write('#GroupID, MJD, PLATEID, FIBERID, RA, DEC, REDSHIFT, FILENAME, ARGS\n')
 
-        DELTA = 2.0/60.0/60.0 #2 arcseconds in degrees
+        DELTA = (2.0/60.0/60.0)**2 #2 arcseconds in degrees
+	delta = 2.0/60.0/60.0 #sqrt(DELTA)
+	OBJECTS = len(X)
         groupedObjects = [] #2D array: first entry in each row is an object, following entries are all objects within DELTA
         countdown = 0
         groupID = 0
-        for i in range(len(X)):
-                if countdown < 0:
-                        countdown = 0
-			print "Negative Countdown Warning!"
+	PLEASE_SKIP = set()
+	def Scribble(obj): #Writes given data to the output file
 
-                if countdown == 0:
-                        countdown += 1
-                        singleGroup = []
-                        singleGroup.append(X[i])
-                        #compare object at X[i] with X[i - 1] and lower, stop when RA diff. is larger than DELTA
+		output.write(", ".join((str(groupID),str(obj['MJD']),str(obj['PLATE']),str(obj['FIBER']),str(obj['RA']),str(obj['DEC']),str(obj['Z']),obj['FILENAME'], 'Not_Interesting\n')))
+
+        for i in xrange(OBJECTS):
+                if i in PLEASE_SKIP:
+                        PLEASE_SKIP.remove(i)
+
+                else:
+                        #singleGroup = [X[i]]
+			Scribble(X[i])
+                        #compare object at X[i] with X[i - 1] and lower, stop when DEC diff. is larger than DELTA
                         j = i - 1
-                        while j >= 0 and X[i]['DEC'] - DELTA < X[j]['DEC']:
-                                if DELTA > math.sqrt((X[i]['cos(RA)']-X[j]['cos(RA)'])**2 + (X[i]['DEC']-X[j]['DEC'])**2):
-                                        singleGroup.append(X[j])
-                                        countdown += 1
+                        while j >= 0 and X[i]['DEC'] - delta < X[j]['DEC']:
+                                if DELTA > (X[i]['cos(RA)']-X[j]['cos(RA)'])**2 + (X[i]['DEC']-X[j]['DEC'])**2:
+                                        #singleGroup.append(X[j])
+					Scribble(X[j])
+                                        PLEASE_SKIP.add(j)
                                 j -= 1
                         #repeat, but with X[i + 1] and higher 
                         j = i + 1
-                        while j < len(X) and X[i]['DEC'] + DELTA > X[j]['DEC']:
-                                if DELTA > math.sqrt((X[i]['cos(RA)']-X[j]['cos(RA)'])**2 + (X[i]['DEC']-X[j]['DEC'])**2):
-                                        singleGroup.append(X[j])
-                                        countdown += 1
+                        while j < OBJECTS and X[i]['DEC'] + delta > X[j]['DEC']:
+                                if DELTA > (X[i]['cos(RA)']-X[j]['cos(RA)'])**2 + (X[i]['DEC']-X[j]['DEC'])**2:
+                                        #singleGroup.append(X[j])
+					Scribble(X[j])
+                                        PLEASE_SKIP.add(j)
                                 j += 1
                         
                         groupID += 1
-			for obj in singleGroup:
+			#for obj in singleGroup:
 
-				output.write(str(groupID) + ', ' + str(obj['MJD'])+', '+str(obj['PLATE'])+', '+str(obj['FIBER'])+', '+str(obj['RA'])+', '+str(obj['DEC'])+', '+str(obj['Z']) + ', ' + obj['FILENAME']+ ', ' + 'Not_Interesting' + '\n')
-
-		countdown -= 1
+			#	output.write(str(groupID) + ', ' + str(obj['MJD'])+', '+str(obj['PLATE'])+', '+str(obj['FIBER'])+', '+str(obj['RA'])+', '+str(obj['DEC'])+', '+str(obj['Z']) + ', ' + obj['FILENAME']+ ', ' + 'Not_Interesting' + '\n')
 
 	output.close()
 	print "Done Matching"
@@ -140,60 +228,29 @@ def saveInterestingObjects(DataArray): #Saves the list of interesting objects
 
 def getMatchesArray(): #Parses the MAtches.rep file into an array of matches spectra
 
-	#Matches = open('Matches.rep','rb')
-
 	try:
 
-		InterestingFile = np.loadtxt('InterestingMatches.rep',delimiter = ', ', dtype={'names':['MJD','PLATEID','FIBERID','RA','DEC','REDSHIFT'], 'formats': ["int","int","int","float","float","float"]})
-
-		InterestingFlag = [str(i['MJD']) + str(i['PLATEID']) + str(i['FIBERID']) for i in InterestingFile]
-
+		InterestingFile = np.loadtxt('InterestingMatches.rep',delimiter = ', ',usecols = (0,1,2), dtype={'names':['MJD','PLATEID','FIBERID'], 'formats': ["<S8","<S8","<S8"]})
+	
+		InterestingFlag = np.core.defchararray.add(np.core.defchararray.add(InterestingFile['MJD'], InterestingFile['PLATEID']), InterestingFile['FIBERID'])
 
 	except:
 
 		InterestingFlag = []
 
-	#matchList = []
-
 	matchList = np.loadtxt("Matches.rep", delimiter=', ', dtype={'names':["GroupID","MJD","PLATEID","FIBERID","RA","DEC","REDSHIFT","FILENAME","Interesting"], 'formats': ["int","int","int","int","float","float","float","<S64","<S64"]})
 
-	for i in matchList:
+	print "Text File Loaded"
 
-		if str(i['MJD'])+str(i['PLATEID'])+str(i['FIBERID']) in InterestingFlag:
+	IDlist = np.core.defchararray.add(np.core.defchararray.add(matchList['MJD'].astype('<S8'), matchList['PLATEID'].astype('<S8')), matchList['FIBERID'].astype('<S8'))
 
-			i['Interesting'] = 'Interesting'
-
-
-	#for line in Matches:
-
-	#	matchList.append(line.split(', '))
-
-
-	#for line in Matches:
-
-	#	matchList.append([])
-		
-	#	matchList[-1].append(int(line[0:line.find(',')])) #gets the group id
-	#	matchList[-1].append([])	
-
-	#	while pos+24 < len(line): #lists all the file names
-	#		matchList[-1][-1].append(line[line.find('{',pos)+1:line.find('},',pos)])
-	#		pos = line.find('},',pos)+2
-
-	#	matchList[-1].append(len(matchList[-1][-1]))
-
-	#	if matchList[-1][0] in InterestingFlag:		
-
-	#		matchList[-1].append('Interesting')
-		
-	#	else:
+	indecies = np.in1d(IDlist, InterestingFlag, assume_unique = True)
 	
-	#		matchList[-1].append('Not_Interesting')
+	matchList['Interesting'][indecies] = 'Interesting'
 
+	print "Number of Interesting Files: ", len(InterestingFlag)
 
-	#	matchList[-1].append(float(line[line.rfind('@')+1:line.rfind(',')]))
-
-	#Matches.close()
+	print "Total Number of Files: ", len(matchList)
 
 	return matchList
 
