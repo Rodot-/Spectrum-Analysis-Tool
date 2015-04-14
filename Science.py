c479ab732dacada8cd73d068b0935beaf722cb9b
Jack O'Brien <jto33@drexel.edu>
2015-04-14 18:19:41 -0400

    Better Tag Handling

--- Science.py
+++ Science.py
@@ -1,23 +1,9 @@
 import numpy as np
 
-CIV = 1545.86
-LyA = 1215.24
-NV = 1239.42
-MgII = 2800.32
-Ha = 6564.61
-Hb = 4863.68
-OII = 3728.30
-OIII = 4960.295
-
-CHARS = {'alpha':"".join((chr(206),chr(177))), 'empty':chr(0),'beta':"".join((chr(206),chr(178)))}
-
-Slines = np.array((CIV, LyA, NV, MgII, Ha, Hb, OII, OIII), dtype = {'names':['CIV:',"".join(('Ly-',CHARS['alpha'],':')),'NV:','MgII:',"".join(('H',CHARS['alpha'],':')),"".join(('H',CHARS['beta'],':')),'OII:','OIII:'], 'formats':['<f8' for i in xrange(8)]})
-
-Slines_Values = np.array([Slines[i] for i in Slines.dtype.names])
-
+TAGS = dict(np.loadtxt('resource/tags.conf', delimiter = ' ', dtype = [('keys','<S8'),('values','<f4')]))
 
 def RedShiftSlines(z): #Returns Spectral lines with redshift corrections
 	
-	return (Slines_Values * (1 + z)).astype(int)
+	return (np.array(TAGS.values()) * (1 + z)).astype(int)
 
 
