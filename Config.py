c0e750b810e6ae91b708fab234ce03e578dd6425
Jack O'Brien <jto33@drexel.edu>
2015-05-05 14:37:04 -0400

    Added Colors to console output.

--- Config.py
+++ Config.py
@@ -1,8 +1,20 @@
 
+class bcolors:
+
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
+
 options = dict()
 with open('resource/SAT.conf', 'rb') as Config:
 
 	for line in Config:
+		if line[0] == '#': continue
 		items = line.split()
 		if len(items) == 3:
 			options.setdefault(items[0], items[2])
